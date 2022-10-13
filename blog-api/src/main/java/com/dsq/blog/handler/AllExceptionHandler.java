25c1e3c3e41142960b32b067e43f8fed5ae9249a
Mechell_Water <1730118383@qq.com>
2022-10-13 11:59:17 +0800

    add .idea to ignore list

--- blog-api/src/main/java/com/dsq/blog/handler/AllExceptionHandler.java
+++ blog-api/src/main/java/com/dsq/blog/handler/AllExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.dsq.blog.handler;
 
 import com.dsq.blog.vo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 //对加了@Controller注解的方法进行拦截处理 AOP的实现
+@Slf4j
 @ControllerAdvice
 public class AllExceptionHandler {
     //进程异常处理，处理Exception.class的异常
@@ -15,4 +20,28 @@ public class AllExceptionHandler {
         ex.printStackTrace();
         return Result.fail(-999, "系统异常");
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Result handler(MethodArgumentNotValidException e){
+        log.error("发生异常: {}",e.getMessage());
+        return Result.fail(e.getBindingResult().getAllErrors().stream().findFirst().get().getDefaultMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Result handler(IllegalArgumentException e){
+        log.error("发生异常: {}",e.getMessage());
+        return Result.fail(e.getMessage());
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(RuntimeException.class)
+    public Result handler(RuntimeException e){
+        log.error("发生异常: {}",e.getMessage());
+        return Result.fail(e.getMessage());
+    }
+
+
 }
--- blog-api/src/main/java/com/dsq/blog/vo/Result.java
+++ blog-api/src/main/java/com/dsq/blog/vo/Result.java
@@ -22,4 +22,46 @@ public class Result {
     public static Result fail(int code, String msg) {
         return new Result(false, code, msg, null);
     }
+
+    public static Result fail(String msg){
+        return Result.fail(500,msg);
+    }
+
+    public static Result fail(CODE code){
+        return Result.fail(code.getNumber(), code.getMsg());
+    }
+
+    /**
+     * 错误返回代码枚举类
+     * @author Mehcell_Water
+     * @date 2022/9/27 14:04
+     */
+    public enum CODE{
+        NOT_FIND_PAGE(404,"未查找到该页面");
+
+
+        private Integer number;
+        private String msg;
+
+        CODE(Integer number, String msg) {
+            this.number = number;
+            this.msg = msg;
+        }
+
+        public Integer getNumber() {
+            return number;
+        }
+
+        public void setNumber(Integer number) {
+            this.number = number;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
+    }
 }
